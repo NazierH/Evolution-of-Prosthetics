4df4ac3e6526de6d159664e227e5ee753d95fec9
Nazier Chand <66347003+NazierC@users.noreply.github.com>
2021-10-06 22:12:01 +1100

    Completed
    Completed webpage.

--- index.js
+++ index.js
@@ -12,34 +12,201 @@ window.addEventListener("scroll", () => {
 });
 
 
+function runAnimationOnScroll(slideNum)
+{
+  document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("scroll", function(event) {
+        const animatedBoxes = document.getElementsByClassName(slideNum);
+        const windowOffsetTop = window.innerHeight + window.scrollY;
+  
+        Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+            const animatedBoxOffsetTop = animatedBox.offsetTop;
+  
+            if (windowOffsetTop >= animatedBoxOffsetTop) {
+              setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+              
+              
+              
+            }
+
+        });
+    });
+    
+  });
+}
 document.addEventListener("DOMContentLoaded", function(event) {
   document.addEventListener("scroll", function(event) {
-      const animatedBoxes = document.getElementsByClassName("slide1");
+      const animatedBoxes = document.getElementsByClassName('slide1');
       const windowOffsetTop = window.innerHeight + window.scrollY;
 
       Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
           const animatedBoxOffsetTop = animatedBox.offsetTop;
 
           if (windowOffsetTop >= animatedBoxOffsetTop) {
-            setTimeout(() => {addClass(animatedBox, "fade-in");}, 50);
+            setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+            
+            
             
           }
+
       });
   });
+  
+});
+document.addEventListener("DOMContentLoaded", function(event) {
   document.addEventListener("scroll", function(event) {
-    const animatedBoxes = document.getElementsByClassName("slide2");
-    const windowOffsetTop = window.innerHeight + window.scrollY;
+      const animatedBoxes = document.getElementsByClassName('slide2');
+      const windowOffsetTop = window.innerHeight + window.scrollY;
 
-    Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
-        const animatedBoxOffsetTop = animatedBox.offsetTop;
+      Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+          const animatedBoxOffsetTop = animatedBox.offsetTop;
 
-        if (windowOffsetTop >= animatedBoxOffsetTop) {
-          setTimeout(() => {addClass(animatedBox, "fade-in");}, 125);
-          
-        }
-    });
+          if (windowOffsetTop >= animatedBoxOffsetTop) {
+            setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+            
+            
+            
+          }
+
+      });
+  });
+  
 });
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.addEventListener("scroll", function(event) {
+      const animatedBoxes = document.getElementsByClassName('slide3');
+      const windowOffsetTop = window.innerHeight + window.scrollY;
+
+      Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+          const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+          if (windowOffsetTop >= animatedBoxOffsetTop) {
+            setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+            
+            
+            
+          }
+
+      });
+  });
+  
 });
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.addEventListener("scroll", function(event) {
+      const animatedBoxes = document.getElementsByClassName('slide4');
+      const windowOffsetTop = window.innerHeight + window.scrollY;
+
+      Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+          const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+          if (windowOffsetTop >= animatedBoxOffsetTop) {
+            setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+            
+            
+            
+          }
+
+      });
+  });
+  
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.addEventListener("scroll", function(event) {
+      const animatedBoxes = document.getElementsByClassName('slide5');
+      const windowOffsetTop = window.innerHeight + window.scrollY;
+
+      Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+          const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+          if (windowOffsetTop >= animatedBoxOffsetTop) {
+            setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+            
+            
+            
+          }
+
+      });
+  });
+  
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.addEventListener("scroll", function(event) {
+      const animatedBoxes = document.getElementsByClassName('slide6');
+      const windowOffsetTop = window.innerHeight + window.scrollY;
+
+      Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+          const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+          if (windowOffsetTop >= animatedBoxOffsetTop) {
+            setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+            
+            
+            
+          }
+
+      });
+  });
+  
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.addEventListener("scroll", function(event) {
+      const animatedBoxes = document.getElementsByClassName('slide7');
+      const windowOffsetTop = window.innerHeight + window.scrollY;
+
+      Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+          const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+          if (windowOffsetTop >= animatedBoxOffsetTop) {
+            setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+            
+            
+            
+          }
+
+      });
+  });
+  
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.addEventListener("scroll", function(event) {
+      const animatedBoxes = document.getElementsByClassName('thanks');
+      const windowOffsetTop = window.innerHeight + window.scrollY;
+
+      Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+          const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+          if (windowOffsetTop >= animatedBoxOffsetTop) {
+            setTimeout(() => {addClass(animatedBox, "fade-in");}, 25);
+            
+            
+            
+          }
+
+      });
+  });
+  
+});
+
+
+// runAnimationOnScroll('slide1');
+// runAnimationOnScroll('slide2');
+// runAnimationOnScroll('slide3');
+// runAnimationOnScroll('slide4');
+
+// document.addEventListener("scroll", function(event) {
+//   const animatedBoxes = document.getElementsByClassName("slide2");
+//   const windowOffsetTop = window.innerHeight + window.scrollY;
+
+//   Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+//       const animatedBoxOffsetTop = animatedBox.offsetTop;
+
+//       if (windowOffsetTop >= animatedBoxOffsetTop) {
+//         setTimeout(() => {addClass(animatedBox, "fade-in");}, 125);
+        
+//       }
+//   });
+// });
 
 function addClass(element, className) {
   const arrayClasses = element.className.split(" ");
